cae9c182be1451e556d33f1c852736cfe1c5e9b8
Kiyong Kim <scgyong@tukorea.ac.kr>
2026-03-11 01:21:37 +0900

    버튼이니 #4 가 가장 어룰리고 간편하고 명확하다.

--- andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/firstapp/MainActivity.kt
+++ andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/firstapp/MainActivity.kt
@@ -2,7 +2,6 @@ package kr.ac.tukorea.ge.scgyong.firstapp
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,17 +16,6 @@ class MainActivity : AppCompatActivity() {
         // 액티비티가 사용할 화면 레이아웃을 연결한다.
         setContentView(R.layout.activity_main)
 
-        val firstButton = findViewById<Button>(R.id.first_button)
-        val secondButton = findViewById<Button>(R.id.second_button)
-
-        // Event Listener 연결하는 방법 #2
-        // 첫 번째 버튼에서 일이 생기면 첫 번째 멤버한테 알려줘.
-        firstButton.setOnClickListener(firstButtonHandler)
-
-        // Event Listener 연결하는 방법 #2
-        // 두 번째 버튼에서 일이 생기면 두 번째 멤버한테 알려줘.
-        secondButton.setOnClickListener(secondButtonHandler)
-
         // 시스템 바 영역만큼 패딩을 적용해서 상태바, 내비게이션 바와 겹치지 않게 한다.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -35,22 +23,18 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-    // Event Listener 연결하는 방법 #2
-    // 첫 번째 버튼에서 일이 생기면 첫 번째 멤버한테 알려줘.
-    private val firstButtonHandler = object : View.OnClickListener {
-        override fun onClick(v: View?) {
-            val lowerTextView = findViewById<TextView>(R.id.lower_text_view)
-            lowerTextView.text = getString(R.string.first_button_clicked)
-        }
+
+    // Event Listener 연결하는 방법 #4
+    // XML의 android:onClick 속성으로 첫 번째 버튼 클릭 메서드를 직접 연결한다.
+    fun onBtnFirstButton(view: View) {
+        val lowerTextView = findViewById<TextView>(R.id.lower_text_view)
+        lowerTextView.text = getString(R.string.first_button_clicked)
     }
 
-    // Event Listener 연결하는 방법 #2
-    // 두 번째 버튼에서 일이 생기면 두 번째 멤버한테 알려줘.
-    private val secondButtonHandler = object : View.OnClickListener {
-        override fun onClick(v: View?) {
-            val lowerTextView = findViewById<TextView>(R.id.lower_text_view)
-            lowerTextView.text = getString(R.string.second_button_clicked)
-        }
+    // Event Listener 연결하는 방법 #4
+    // XML의 android:onClick 속성으로 두 번째 버튼 클릭 메서드를 직접 연결한다.
+    fun onBtnSecondButton(view: View) {
+        val lowerTextView = findViewById<TextView>(R.id.lower_text_view)
+        lowerTextView.text = getString(R.string.second_button_clicked)
     }
-
 }
