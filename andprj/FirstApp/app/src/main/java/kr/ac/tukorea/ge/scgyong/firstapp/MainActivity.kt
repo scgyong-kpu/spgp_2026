3d1ee8208eb8ce170059ca4474336e8965cc45b3
Kiyong Kim <scgyong@tukorea.ac.kr>
2026-03-11 01:08:24 +0900

    #3 방법은 관련된 코드끼리 가까이 둘 수 있다는 장점이 있지만 onCreate() 함수가 너무 길어진다.

--- andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/firstapp/MainActivity.kt
+++ andprj/FirstApp/app/src/main/java/kr/ac/tukorea/ge/scgyong/firstapp/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.tukorea.ge.scgyong.firstapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,23 +17,31 @@ class MainActivity : AppCompatActivity() {
         // 액티비티가 사용할 화면 레이아웃을 연결한다.
         setContentView(R.layout.activity_main)
 
+        val lowerTextView = findViewById<TextView>(R.id.lower_text_view)
+        val firstButton = findViewById<Button>(R.id.first_button)
+        val secondButton = findViewById<Button>(R.id.second_button)
+
+        // Event Listener 연결하는 방법 #3
+        // 즉석에서 객체를 만들어 전달해 줄테니 얘한테 알려줘.
+        firstButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                lowerTextView.text = getString(R.string.first_button_clicked)
+            }
+        })
+
+        // Event Listener 연결하는 방법 #3
+        // 즉석에서 객체를 만들어 전달해 줄테니 얘한테 알려줘.
+        secondButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                lowerTextView.text = getString(R.string.second_button_clicked)
+            }
+        })
+
         // 시스템 바 영역만큼 패딩을 적용해서 상태바, 내비게이션 바와 겹치지 않게 한다.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    // XML의 android:onClick 속성을 통해 호출되는 첫 번째 버튼 클릭 처리 메서드이다.
-    fun onBtnFirstButton(view: View) {
-        val lowerTextView = findViewById<TextView>(R.id.lower_text_view)
-        lowerTextView.text = getString(R.string.first_button_clicked)
-    }
-
-    // XML의 android:onClick 속성을 통해 호출되는 두 번째 버튼 클릭 처리 메서드이다.
-    fun onBtnSecondButton(view: View) {
-        val lowerTextView = findViewById<TextView>(R.id.lower_text_view)
-        lowerTextView.text = getString(R.string.second_button_clicked)
     }
 }
