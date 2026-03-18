9f11f86ae88621d14d848cbe2016277d93e63631
none <none>
2026-03-18 10:00:07 +0900

    카드 버튼 클릭 이벤트 처리 추가 및 Logcat 디버깅 소개
        - 여러 ImageButton이 동일한 onClick 함수로 전달되는 것을 확인하기 위해 클릭된 View의 id를 Log로 출력
        - Log.d(tag, message) 형식의 Android 로그 사용과 Logcat 창에서 로그를 확인하는 방법 설명
        - 로그 메시지에서 Kotlin String Interpolation($변수, ${수식}) 사용 예시 추가
        - ID 를 부여하지 않으면 -1 로 나옴

--- andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa02/MainActivity.kt
+++ andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa02/MainActivity.kt
@@ -1,6 +1,8 @@
 package kr.ac.tukorea.ge.spgp.scgyong.cardsa02
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kr.ac.tukorea.ge.spgp.scgyong.cardsa02.databinding.ActivityMainBinding
 
@@ -12,4 +14,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+    fun onCardClicked(view: View) {
+        Log.d("MainActivity", "ImageButton Clicked! ID=${view.id}")
+    }
+
 }
