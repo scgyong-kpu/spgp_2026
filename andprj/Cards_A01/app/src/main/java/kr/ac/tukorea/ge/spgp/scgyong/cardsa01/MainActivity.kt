c65f90561df7d56629cb3d604b2ad474613cefb8
none <none>
2026-03-18 12:12:02 +0900

    Toast 를 써 볼 수도 있다. 이 프로젝트에서는 로그를 Toast 로 보여 주지는 않을 거지만, 사용법을 알아두자.

--- andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa01/MainActivity.kt
+++ andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa01/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.tukorea.ge.spgp.scgyong.cardsa01
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kr.ac.tukorea.ge.spgp.scgyong.cardsa01.databinding.ActivityMainBinding
 
@@ -16,8 +17,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onCardButtonClick(view: View) {
-        val name = resources.getResourceName(view.id)
-        val msg = "Clicked: $name(${view.id})"
+//        val name = resources.getResourceName(view.id)
+//        val msg = "Clicked: $name(${view.id})"
+        val msg = getString(R.string.card_clicked_fmt, view.id)
         Log.d("MainActivity", msg)
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
