c9528585fc34fcf30cd03c95a1ea0b2dbb46018a
none <none>
2026-03-18 10:51:48 +0900

    이전 카드 인덱스를 이용한 카드 뒤집기 로직 구현     - 이전 카드 버튼 참조 대신 openedCardIndex:Int? 변수로 열린 카드 위치를 저장하도록 변경     - 카드가 클릭되면 이전 카드가 존재할 경우 cardButtons[openedCardIndex]를 이용해 해당 버튼을 찾아 뒷면 이미지로 되돌림     - 현재 클릭된 버튼 이미지를 Ace of Spades로 변경     - cardButtons.indexOf(button)을 사용하여 클릭된 버튼의 배열 인덱스를 계산하고 openedCardIndex에 저장
        이전 카드 처리 로직을 Kotlin 스타일로 개선
        - openedCardIndex != null 검사 대신 openedCardIndex?.let {} 사용
        - null 체크 후 사용하던 패턴을 Kotlin null-safe 방식으로 단순화
        - !! 연산자를 제거하여 코드 안정성과 가독성 개선
        - 버튼 객체(ImageButton) 대신 인덱스(Int)로 상태를 관리하는 방식으로 구조를 정리하여 이후 게임 상태 관리와 저장/복원에 유리하도록 설계

--- andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa02/MainActivity.kt
+++ andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa02/MainActivity.kt
@@ -8,12 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import kr.ac.tukorea.ge.spgp.scgyong.cardsa02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var previousButton: ImageButton? = null
+    private var openedIndex: Int? = null
     private lateinit var binding: ActivityMainBinding
+    private lateinit var buttons: Array<ImageButton>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        buttons = arrayOf(
+            binding.card00, binding.card01, binding.card02, binding.card03,
+            binding.card10, binding.card11, binding.card12, binding.card13,
+            binding.card20, binding.card21, binding.card22, binding.card23,
+            binding.card30, binding.card31, binding.card32, binding.card33,
+        )
         setContentView(binding.root)
     }
 
@@ -21,11 +28,15 @@ class MainActivity : AppCompatActivity() {
         val msg = getString(R.string.card_clicked_fmt, view.id)
         Log.d("MainActivity", msg)
         val button = view as ImageButton
+        val buttonIndex = buttons.indexOf(button)
+
         button.setImageResource(R.mipmap.card_as)
         //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 
-        previousButton?.setImageResource(R.mipmap.card_blue_back)
-        previousButton = button
+        openedIndex?.let { index ->
+            buttons[index].setImageResource(R.mipmap.card_blue_back)
+        }
+        openedIndex = buttonIndex
     }
 
 }
