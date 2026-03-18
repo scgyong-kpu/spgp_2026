4c0aac4ddf4bc94af15a1b3b88cc410924d31915
none <none>
2026-03-18 12:12:48 +0900

    Toast 는 삭제한다.

--- andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa01/MainActivity.kt
+++ andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa01/MainActivity.kt
@@ -3,7 +3,6 @@ package kr.ac.tukorea.ge.spgp.scgyong.cardsa01
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kr.ac.tukorea.ge.spgp.scgyong.cardsa01.databinding.ActivityMainBinding
 
@@ -21,6 +20,6 @@ class MainActivity : AppCompatActivity() {
 //        val msg = "Clicked: $name(${view.id})"
         val msg = getString(R.string.card_clicked_fmt, view.id)
         Log.d("MainActivity", msg)
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
