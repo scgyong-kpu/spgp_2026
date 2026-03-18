70855dfcc3615a2713559fac5d048a1669055663
none <none>
2026-03-18 12:57:01 +0900

    이전 카드 인덱스를 이용한 카드 뒤집기 로직 구현     - 이전 카드 버튼 참조 대신 openedCardIndex:Int? 변수로 열린 카드 위치를 저장하도록 변경     - 카드가 클릭되면 이전 카드가 존재할 경우 cardButtons[openedCardIndex]를 이용해 해당 버튼을 찾아 뒷면 이미지로 되돌림     - 현재 클릭된 버튼 이미지를 Ace of Spades로 변경     - cardButtons.indexOf(button)을 사용하여 클릭된 버튼의 배열 인덱스를 계산하고 openedCardIndex에 저장     - 버튼 객체(ImageButton) 대신 인덱스(Int)로 상태를 관리하는 방식으로 구조를 정리하여 이후 게임 상태 관리와 저장/복원에 유리하도록 설계

--- andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa01/MainActivity.kt
+++ andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/cardsa01/MainActivity.kt
@@ -1,29 +1,40 @@
 package kr.ac.tukorea.ge.spgp.scgyong.cardsa01
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import kr.ac.tukorea.ge.spgp.scgyong.cardsa01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var previousImageButton: ImageButton? = null
+    private var openedCardIndex: Int? = null
     private lateinit var binding: ActivityMainBinding
+    private lateinit var buttons: Array<ImageButton>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        buttons = arrayOf(
+            binding.card00, binding.card01, binding.card02, binding.card03,
+            binding.card10, binding.card11, binding.card12, binding.card13,
+            binding.card20, binding.card21, binding.card22, binding.card23,
+            binding.card30, binding.card31, binding.card32, binding.card33,
+        )
     }
 
     fun onCardButtonClick(view: View) {
         //val button = view as? ImageButton ?: return
         val button = view as ImageButton
+        val buttonIndex = buttons.indexOf(button)
 
         button.setImageResource(R.mipmap.card_as)
 
-        previousImageButton?.setImageResource(R.mipmap.card_blue_back)
-        previousImageButton = button
+        if (openedCardIndex != null) {
+            val openedButton = buttons[openedCardIndex!!]
+            openedButton.setImageResource(R.mipmap.card_blue_back)
+        }
+        openedCardIndex = buttonIndex
     }
 }
