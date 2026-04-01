4898f0df95befae6b6740cea5f23ffe736ad36b6
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-04-01 09:07:45 +0900

    DragonFlightActivity 가 BaseGameActivity 를 상속받도록 정리
    - DragonFlightActivity 가 AppCompatActivity 대신 BaseGameActivity 를 상속받도록 변경한다
    - createRootScene(gctx) 를 구현하고, 아직 MainScene 파일을 만들지 않은 상태이므로 가장 작은 형태의 anonymous Scene 을 root scene 으로 반환한다
    - 이렇게 하면 a2dg 의 GameView / Scene 흐름으로 진입하는 단계와 MainScene 생성 단계를 작은 commit 두 개로 나눌 수 있다
    - README 체크리스트에서 DragonFlight 에 a2dg 모듈 연결 항목과 DragonFlightActivity 가 BaseGameActivity 를 상속 항목을 완료 처리한다

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2026/dragonflight/DragonFlightActivity.kt
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2026/dragonflight/DragonFlightActivity.kt
@@ -1,13 +1,17 @@
 package kr.ac.tukorea.ge.spgp2026.dragonflight
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kr.ac.tukorea.ge.spgp2026.a2dg.activity.BaseGameActivity
+import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
+import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
 
-class DragonFlightActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class DragonFlightActivity : BaseGameActivity() {
+    override fun createRootScene(gctx: GameContext): Scene {
+        // 이번 단계의 목표는 DragonFlightActivity 가 BaseGameActivity 를 상속받아
+        // a2dg 의 GameView / Scene 흐름으로 진입하는 것까지 확인하는 데 있다.
+        //
+        // 그래서 아직은 MainScene 파일을 따로 만들지 않고,
+        // 가장 작은 형태인 anonymous Scene 을 root scene 으로 반환한다.
+        return object : Scene(gctx) {
+        }
     }
 }
