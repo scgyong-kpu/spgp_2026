43082bcd41e1ed1aca56b6df173419823a3b5d69
KYKim@Home <scgyong@tukorea.ac.kr>
2026-04-25 20:08:32 +0900

    BG 3장 추가하여 왼쪽으로 각기 다른 속도로 스크롤되게 함

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2026/cookierun/game/main/MainScene.kt
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2026/cookierun/game/main/MainScene.kt
@@ -1,7 +1,16 @@
 package kr.ac.tukorea.ge.spgp2026.cookierun.game.main
 
+import kr.ac.tukorea.ge.spgp2026.a2dg.objects.HorzScrollBackground
 import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
+import kr.ac.tukorea.ge.spgp2026.a2dg.scene.World
 import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+import kr.ac.tukorea.ge.spgp2026.cookierun.R
 
 class MainScene(gctx: GameContext) : Scene(gctx) {
+    override val clipsRect = true
+    override val world = World(arrayOf(0)).apply {
+        add(HorzScrollBackground(gctx, R.mipmap.cookie_run_bg_1, -50f), 0)
+        add(HorzScrollBackground(gctx, R.mipmap.cookie_run_bg_2, -100f), 0)
+        add(HorzScrollBackground(gctx, R.mipmap.cookie_run_bg_3, -150f), 0)
+    }
 }
