065388d04a6b707febd514fa2dbe7dba6cf46c21
none <none>
2026-05-11 16:11:56 +0900

    onDraw 를 구현하여 path 생성후 그리는 코드를 추가

--- andprj/SmoothingPath01/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/smoothingpath01/PathView.kt
+++ andprj/SmoothingPath01/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/smoothingpath01/PathView.kt
@@ -1,11 +1,15 @@
 package kr.ac.tukorea.ge.spgp.scgyong.smoothingpath01
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.graphics.PointF
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.graphics.toColorInt
 
 class PathView @JvmOverloads constructor(
     context: Context,
@@ -26,4 +30,26 @@ class PathView @JvmOverloads constructor(
 
         return super.onTouchEvent(event)
     }
+
+    override fun onDraw(canvas: Canvas) {
+
+        if (points.size < 2) return
+        val path = Path()
+        val pt = points[0]
+        path.moveTo(pt.x, pt.y)
+        for (i in 1 ..< points.size) {
+            val pt = points[i]
+            path.lineTo(pt.x, pt.y)
+        }
+
+        val paint = Paint()
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 2f
+        paint.color = "#1239FE".toColorInt()
+
+        canvas.drawPath(path, paint)
+//        canvas.draw(path)
+        super.onDraw(canvas)
+    }
+
 }
