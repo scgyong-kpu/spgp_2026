726add4795516a21b4bdbf97ff2cb43774c9439b
none <none>
2026-05-11 14:03:05 +0900

    TOUCH_DOWN 받아서 points array 에 추가

--- andprj/SmoothingPath02/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/smoothingpath02/PathView.kt
+++ andprj/SmoothingPath02/app/src/main/java/kr/ac/tukorea/ge/spgp/scgyong/smoothingpath02/PathView.kt
@@ -1,7 +1,9 @@
 package kr.ac.tukorea.ge.spgp.scgyong.smoothingpath02
 
 import android.content.Context
+import android.graphics.PointF
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class PathView @JvmOverloads constructor(
@@ -9,4 +11,17 @@ class PathView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
 ): View(context, attrs, defStyleAttr) {
+    var points = arrayListOf<PointF>()
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action != MotionEvent.ACTION_DOWN) {
+            return false
+        }
+        //float x = event.getX()
+        val x = event.x
+        val y = event.y
+        val pt = PointF(x, y)
+        points.add(pt)
+
+        return super.onTouchEvent(event)
+    }
 }
