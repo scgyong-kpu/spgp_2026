c47314a0b3e3dc018dcc425b9576d12dd27ef695
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-05-12 22:32:49 +0900

    MainActivity package(폴더)를 .app 으로 옮김.

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2026/tudefence/app/MainActivity.kt
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2026/tudefence/app/MainActivity.kt
@@ -1,10 +1,11 @@
-package kr.ac.tukorea.ge.spgp2026.tudefence
+package kr.ac.tukorea.ge.spgp2026.tudefence.app
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kr.ac.tukorea.ge.spgp2026.tudefence.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
