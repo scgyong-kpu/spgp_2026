b290a222b6dc8e5e01bba99ac0273c9ae95e210e
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-05-12 22:39:56 +0900

    MainActivity 에 ViewBinding 적용
    - ActivityMainBinding 을 사용해 layout inflate 결과를 binding 으로 보관
    - setContentView() 가 layout resource id 대신 binding.root 를 사용하도록 변경
    - 이후 view 접근을 findViewById 없이 binding 기반으로 진행할 수 있게 준비

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2026/tudefence/app/MainActivity.kt
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2026/tudefence/app/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kr.ac.tukorea.ge.spgp2026.tudefence.R
+import kr.ac.tukorea.ge.spgp2026.tudefence.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
