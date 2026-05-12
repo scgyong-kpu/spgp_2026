d3dc5053bb1a4b6371d7c2cf61ccf697f48711bf
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-05-12 22:48:33 +0900

    MainGameActivity 를 BaseGameActivity 기반으로 전환
    - AppCompatActivity 상속과 기본 Android view 초기화 코드를 제거
    - a2dg 의 BaseGameActivity 를 상속해 게임 루프와 Scene 구성을 사용할 수 있게 변경
    - createRootScene() 에서 임시 빈 Scene 을 반환해 게임 Activity 실행 흐름을 먼저 확인할 수 있게 준비

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2026/tudefence/app/MainGameActivity.kt
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2026/tudefence/app/MainGameActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kr.ac.tukorea.ge.spgp2026.a2dg.activity.BaseGameActivity
+import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
+import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
 import kr.ac.tukorea.ge.spgp2026.tudefence.R
 
-class MainGameActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainGameActivity : BaseGameActivity() {
+    override fun createRootScene(gctx: GameContext): Scene {
+        return object:Scene(gctx) {}
     }
 }
