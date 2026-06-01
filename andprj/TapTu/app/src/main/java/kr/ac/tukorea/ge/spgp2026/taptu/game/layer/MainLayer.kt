017440ea8a9fe2ef79a58960cc68d20581a9e51a
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-06-02 05:34:18 +0900

    MainScene 에 배경 layer 추가
    - MainLayer 에 BG layer 를 정의하고 MainScene 이 World 를 가지도록 한다
    - 선택된 곡 정보를 MainScene 에서 참조하도록 준비한다
    - bg 이미지를 배경 Sprite 로 추가하고 화면 폭에 맞게 배치한다

---
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2026/taptu/game/layer/MainLayer.kt
@@ -0,0 +1,5 @@
+package kr.ac.tukorea.ge.spgp2026.taptu.game.layer
+
+enum class MainLayer {
+    BG,
+}
--- andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2026/taptu/game/scene/main/MainScene.kt
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2026/taptu/game/scene/main/MainScene.kt
@@ -1,10 +1,23 @@
 package kr.ac.tukorea.ge.spgp2026.taptu.game.scene.main
 
+import kr.ac.tukorea.ge.spgp2026.a2dg.objects.Sprite
 import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
+import kr.ac.tukorea.ge.spgp2026.a2dg.scene.World
 import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+import kr.ac.tukorea.ge.spgp2026.taptu.R
+import kr.ac.tukorea.ge.spgp2026.taptu.data.SongCatalog
+import kr.ac.tukorea.ge.spgp2026.taptu.game.layer.MainLayer
 
 class MainScene(
     gctx: GameContext,
     songIndex: Int,
 ) : Scene(gctx) {
+    override val world = World<MainLayer>(MainLayer.entries.toTypedArray())
+    val song = SongCatalog.songs[songIndex]
+
+    override fun onEnter() {
+        val bg = Sprite(gctx, R.mipmap.bg)
+        bg.setCenterProportionalWidth(gctx.metrics.width / 2, gctx.metrics.height / 2, gctx.metrics.width)
+        world.add(bg, MainLayer.BG)
+    }
 }
